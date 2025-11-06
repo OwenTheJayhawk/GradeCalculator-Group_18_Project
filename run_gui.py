41d2d027819c96d12cfa88b078f201fda3d040b3
Owen Deines <143202010+OwenTheJayhawk@users.noreply.github.com>
2025-11-06 05:17:37 +0000

    clean-up

--- run_gui.py
+++ run_gui.py
@@ -1,12 +1,9 @@
-"""Launcher for the PyQt5 GUI.
 
-Run this file to start the GUI. If PyQt5 is not installed it will print an instruction.
-"""
 import sys
 import os
 
 if __name__ == '__main__':
-    # Make sure the GUI module is importable
+    
     root = os.path.dirname(__file__)
     if root not in sys.path:
         sys.path.insert(0, root)
